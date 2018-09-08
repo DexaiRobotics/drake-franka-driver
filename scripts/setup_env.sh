f1a8428e410e3ac7050df931e5e83945e05beedb
David M.S. Johnson <dave@dexai.com>
2018-09-08 15:02:56 -0400

    towards using dracula

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -3,3 +3,7 @@
 
 export DRAKE_BUILD_DIR="/opt/drake"
 export FRANKA_BUILD_DIR="$PWD/externals/libfranka/build"
+export DRACULA_SOURCE_PATH="$PWD/../dracula"
+export DRACULA_BUILD_PATH="$DRACULA_SOURCE_PATH/build"
+export DRACULA_INCLUDE_DIR="$DRACULA_SOURCE_PATH/dracula/include"
+export DRACULA_LIBRARIES="$DRACULA_BUILD_PATH/dracula/libdracula.so"
