3bc507e6763cdaf7bea6f9b6cfaba10ee24fcee7
Syler <syler@dexai.com>
2019-07-30 16:49:50 -0400

    Replace killall with pkill for docker compatibility

--- franka_0.sh
+++ franka_0.sh
@@ -5,6 +5,6 @@
 config=$HOME/catkin_ws/src/salad_bar_description/franka_0.yaml
 
 # if another driver instance is still somehow running, kill it silently
-killall franka_plan_runner > /dev/null 2>&1
+pkill -f franka_plan_runner > /dev/null 2>&1
 
 ./build/franka_plan_runner $config
