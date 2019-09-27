6d89f41ffd2b8b30a06e2582feeebf63cd10748b
Dexai <alfred@alfred.local>
2019-09-27 15:03:09 -0400

    change franka 0 to franka G

--- franka_G.sh
+++ franka_G.sh
@@ -2,7 +2,7 @@
 
 # TODO: @robert set version in yaml & query
 # TODO: @robert source yaml from host machine via web rather than github
-config=$HOME/catkin_ws/src/salad_bar_description/franka_0.yaml
+config=$HOME/catkin_ws/src/salad_bar_description/franka_G.yaml
 
 # if another driver instance is still somehow running, kill it silently
 pkill -f franka_plan_runner > /dev/null 2>&1
