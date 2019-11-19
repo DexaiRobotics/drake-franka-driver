aeab3385a34b30e9f3f947521712b9514e63b19b
Robert Katzschmann <robert@dexai.com>
2019-11-18 23:02:11 -0500

    clean includes for examples_common

--- include/examples_common.h
+++ include/examples_common.h
@@ -2,14 +2,9 @@
 // Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #pragma once
 
-#include <array>
-
+#include <array>  // for array
 #include <Eigen/Core>
-
-#include <franka/control_types.h>
-#include <franka/duration.h>
 #include <franka/robot.h>
-#include <franka/robot_state.h>
 
 /**
  * @file examples_common.h
