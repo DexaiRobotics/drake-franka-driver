512ac7c443461162eaab8e8e878ff88de2f0dd78
Syler <syler@dexai.com>
2020-07-30 13:29:14 -0400

    Add pushd to franka_sim.sh so script can be run from anywhere

--- franka_sim.sh
+++ franka_sim.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+pushd /src/drake-franka-driver
+
 config=franka_sim.yaml
 
 # if another driver instance is still somehow running, kill it silently
 pkill -f franka_driver > /dev/null 2>&1
 
 ./build/franka_driver $config
+
+popd
