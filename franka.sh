1a1f52cd295cfc91b73153e7e28f187105a3f381
Syler <syler@dexai.com>
2020-09-05 16:01:37 -0400

    Consolidate franka and franka_sim.sh into single script

--- franka.sh
+++ franka.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
-#set -euxo
 
-config=franka.yaml
+pushd /src/drake-franka-driver
 
-echo "choose config file: $config"
+if [[ ! -v SIM_ROBOT ]]; then # SIM_ROBOT environment variable not set
+    config=franka.yaml
+else
+    config=franka_sim.yaml
+fi
 
-echo "start franka_driver"
+# if another driver instance is still somehow running, kill it silently
+pkill -f franka_driver > /dev/null 2>&1
+
 if ./build/franka_driver $config ; then
     echo "franka_driver started successfully"
 else
     echo "franka_driver failed to start"
 fi
+
+popd
