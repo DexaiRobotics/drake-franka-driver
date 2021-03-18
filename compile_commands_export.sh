56563b9deb0f92e1d96be95b2f631e2e32b97462
Duan Yutong <duan.yutong@dexai.com>
2021-03-18 16:14:12 +0000

    update pre-commit hooks

--- compile_commands_export.sh
+++ compile_commands_export.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Copyright © 2020 Dexai Robotics. All rights reserved.
+
 # this script configures the cmake project and generates
 # two versions of compile_commands.json
 # one is the original version, which has triple escapes by ROS
@@ -8,17 +10,19 @@
 # becomes -DROS_PACKAGE_NAME=\"robot_interface\"
 
 src_dir="$(dirname $(realpath $0))"
-
-cmake \
--D CMAKE_BUILD_TYPE:STRING=Debug \
--D CMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc-7 \
--D CMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++-7 \
--H"${src_dir}" \
--B"${src_dir}/build" \
--G "Unix Makefiles"
-
-echo "Fixing escapes in ROS macros..."
 pushd "${src_dir}/build" > /dev/null
+
+if [ ! -f "./compile_commands.json" ]; then
+  cmake \
+    -D CMAKE_BUILD_TYPE:STRING=Debug \
+    -D CMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc-10 \
+    -D CMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++-10 \
+    -H ".." \
+    -B "." \
+    -G "Unix Makefiles"
+fi
+
+echo "Fixing escapes in ROS macros for cppcheck..."
 # using . as delimiters, every first \ is an escape character
 sed 's.\\\\\\\"..g' "compile_commands.json" > "compile_commands_escape.json"
 echo "Compile commands exported."
